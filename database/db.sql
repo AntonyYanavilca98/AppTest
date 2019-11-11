639dcbec764babc30ee6cb1916197b80aa77eef6
Antony <antony.yanavilca98x@gmail.com>
2019-11-11 10:41:11 -0500

    first commit

---
+++ database/db.sql
@@ -0,0 +1,17 @@
+
+CREATE DATABASE AppNodejsMysql;
+
+
+use AppNodejsMysql;
+
+
+CREATE TABLE customer (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(15)
+);
+
+
+
+
